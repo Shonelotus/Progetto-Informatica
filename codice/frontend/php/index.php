5ada9c69fd7b7fcff4789f877ed56ab42e4bcd7f
Shonelotus <morettonicolo6@gmail.com>
2024-05-20 12:07:44 +0200

    Aggiunto file con chiavi e impostato gitIgnore

--- codice/frontend/php/index.php
+++ codice/frontend/php/index.php
@@ -1,18 +1,22 @@
 <?php
-  /*if(!isset($_SESSION))
-  {
-      session_start();
-  }
+    /*if(!isset($_SESSION))
+    {
+        session_start();
+    }
 
-  if(!isset($_SESSION["id"]))
-  {
-      session_destroy();
-  }
-  else
-  {
+    if(!isset($_SESSION["id"]))
+    {
+        session_destroy();
+    }
+    else
+    {
         //devo reindirizzare ad adminpage
         header('Location: index.php');
-  }*/
+    }*/
+
+    $key = parse_ini_file('key.ini');
+
+    $tokenKey = $key['MAPS_KEY'];
 ?>
 
 <!DOCTYPE html>
@@ -69,9 +73,12 @@
     </style>
     <script type="module" src="../javascript/map.js"></script>
     <script>
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6q_-ycy_AMekLba8H6gXnoYI92EhMuXY&loading=async&callback=initMap">
+
+        var tokenKey = "<?php echo $tokenKey; ?>";
+
+        src="https://maps.googleapis.com/maps/api/js?key="+tokenKey+"&loading=async&callback=initMap">
               (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-                key: "AIzaSyC6q_-ycy_AMekLba8H6gXnoYI92EhMuXY",
+                key: tokenKey,
                 v: "weekly",
                 // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
                 // Add other bootstrap parameters as needed, using camel case.
